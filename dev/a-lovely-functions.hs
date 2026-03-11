3edb3d7e8bc4e45ef23e66420339e71f61d63149
Mairead Meagher <mmeagher@wit.ie>
2026-03-11 07:32:39 +0000

    remove question in foldr exercises

--- dev/a-lovely-functions.hs
+++ dev/a-lovely-functions.hs
@@ -6,12 +6,15 @@ clamp = max 0 . min 1
 -- uses recursion to shuffle a list, 
 -- which is not the most efficient way to do it, 
 -- but it's a lovely function nonetheless
- import System.Random
+-- import System.Random
 
-shuffle :: [a] -> IO [a]
-shuffle [] = return []
-shuffle xs = do
-    i <- randomRIO (0, length xs - 1)
-    let (front,(a:back)) = splitAt i xs
-    rest <- shuffle (front ++ back)
-    return (a : rest)
+-- shuffle :: [a] -> IO [a]
+-- shuffle [] = return []
+-- shuffle xs = do
+--     i <- randomRIO (0, length xs - 1)
+--     let (front,(a:back)) = splitAt i xs
+--     rest <- shuffle (front ++ back)
+--     return (a : rest)
+    
+minlistr ::  [Int] -> Int
+minlistr  = foldr min maxBound
